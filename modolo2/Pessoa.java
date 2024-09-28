bfd4f639774c500587dd5b62a4013d9feb22bdbb
filmes favoritos <lucas.rodrigues@pdbomdespacho.com.br>
2024-09-28 11:29:17 -0300

    Update Pessoa.java

--- modolo2/Pessoa.java
+++ modolo2/Pessoa.java
@@ -1,1 +1,17 @@
+classe pública Pessoa {
+    String protegida nome;
+    Livro protegido[] livros;
 
+    public Pessoa(String nome) {
+        this.nome = nome;
+        this.livros = new Livro[0]; // inicialmente, sem livros
+    }
+
+    pública String getNome() {
+        retornar nome;
+    }
+
+    público Livro[] getLivros() {
+        retornar livros;
+    }
+}
